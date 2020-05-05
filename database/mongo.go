e842ce04d2c6ed5d65f3706b624e447108e55e53
a233057 <meluleki.madlala@standardbank.co.za>
2020-05-05 19:24:27 +0200

    test connection to the database

--- database/mongo.go
+++ database/mongo.go
@@ -1,1 +1,31 @@
 package database
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"time"
+)
+
+func NewConnection() (*mongo.Client, error)  {
+	log.Println("Stating Database")
+
+	mongoUrl := mongoConnectionString()
+	var client *mongo.Client
+
+	if mongoUrl == "" {
+		log.Println("No Connection String Provided!")
+	} else {
+		var err error
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		client, err = mongo.Connect(ctx, options.Client().ApplyURI(mongoUrl))
+
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	return client,nil
+}
--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"goMongoService/database"
+	"log"
+	"time"
+)
 
 func main()  {
-	fmt.Println("Hello World...")
+	var db *mongo.Client
+	var err error
+
+	for true {
+		db, err = database.NewConnection()
+		if err != nil {
+			fmt.Println("Failed to access MongoDB. Retrying in 60 seconds")
+			time.Sleep(1 * time.Minute)
+		} else {
+			break
+		}
+	}
+	err = db.Ping(context.Background(), readpref.Primary())
+	if err != nil {
+		log.Fatal("Couldn't connect to the database", err)
+	} else {
+		log.Println("Connected!")
+	}
 }
