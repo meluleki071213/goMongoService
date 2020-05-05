1c9fcb0205b5d15ea91288c82c0c6cd465999d75
a233057 <meluleki.madlala@standardbank.co.za>
2020-05-05 19:50:40 +0200

    Adding models

--- main.go
+++ main.go
@@ -1,32 +1,24 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"goMongoService/database"
-	"log"
 	"time"
 )
 
 func main()  {
-	var db *mongo.Client
+	var _ *mongo.Client
 	var err error
 
 	for true {
-		db, err = database.NewConnection()
+		_, err = database.NewConnection()
 		if err != nil {
 			fmt.Println("Failed to access MongoDB. Retrying in 60 seconds")
 			time.Sleep(1 * time.Minute)
 		} else {
+			fmt.Println("Database Connected")
 			break
 		}
-	}
-	err = db.Ping(context.Background(), readpref.Primary())
-	if err != nil {
-		log.Fatal("Couldn't connect to the database", err)
-	} else {
-		log.Println("Connected!")
 	}
 }
